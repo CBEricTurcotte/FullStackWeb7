1ebeb96d18dff33f23c3b10c226dc55725a3cf5c
Eric Turcotte <eric.turcotte@codeboxx.biz>
2024-03-18 10:12:17 -0400

    new test url

--- mern/client/cypress.config.js
+++ mern/client/cypress.config.js
@@ -6,6 +6,6 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: "http://localhost:5173/", // Set the base URL under e2e configuration
+    baseUrl: "http://localhost:5173", // Set the base URL under e2e configuration
   },
 });
